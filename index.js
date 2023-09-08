1454d2eccda1a1bb6c18bff85a725d6054b35abe
gavielskee <gavielskee1@gmail.com>
2023-09-07 23:45:46 -0400

    edited out function due to inquirer error.

--- index.js
+++ index.js
@@ -28,20 +28,20 @@ inquirer.prompt([
     }
 ])
 
-function writeToFile(fileName, answers) {
-    fs.writeFile(fileName, answers, () => console.log('Generated logo.svg!'))
-}
+// function writeToFile(fileName, answers) {
+//     fs.writeFile(fileName, answers, () => console.log('Generated logo.svg!'))
+// }
 
-function init() {
-    inquirer
-        .prompt(questions)
-        .then((answers) => {
-            const svg = new SVG();
-            svg.setText(answers.logoText, answers.logoTextColor)
-            const shape = eval(`new ${answers.logoShape}()`)
-            shape.setColor(answers.logoShapeColor)
-            svg.setShape(shape)
-            writeToFile('./examples/logo.svg', svg.render())
-        })
-}
-init()
+// function init() {
+//     inquirer
+//         .prompt(questions)
+//         .then((answers) => {
+//             const svg = new SVG();
+//             svg.setText(answers.logoText, answers.logoTextColor)
+//             const shape = eval(`new ${answers.logoShape}()`)
+//             shape.setColor(answers.logoShapeColor)
+//             svg.setShape(shape)
+//             writeToFile('./examples/logo.svg', svg.render())
+//         })
+// }
+// init()
